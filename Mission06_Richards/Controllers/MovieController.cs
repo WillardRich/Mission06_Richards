32130406eae9fcd6e4ae697ce2b3842890ef2390
WillardRich <wrichar4@byu.edu>
2026-02-13 22:29:38 -0700

    Add project files.

---
+++ Mission06_Richards/Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mission06_Richards.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission06_Richards/Data/MovieContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mission06_Richards.Models;
+using System.Collections.Generic;
+
+namespace Mission06_Richards.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
